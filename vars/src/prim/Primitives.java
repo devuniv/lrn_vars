4eb6d813f2d18da5df861a537002458c6180876c
Andrew <codeuniv@mail.ru>
2016-10-22 16:56:10 +0300

    initial commit

---
+++ vars/src/prim/Primitives.java
@@ -0,0 +1,94 @@
+package prim;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class Primitives {
+
+	// Primitives
+	public static byte bbyte = 10;
+	public static short shshort = 20;
+	public static int iint = 100;
+	public static long llong = Integer.MAX_VALUE + 900;
+	public static float ffloat = 66.99f;
+	public static double ddouble = 77.999;
+
+	public static byte bbyte1 = 11;
+	public static short shshort1 = 21;
+	public static int iint1 = 111;
+	public static long llong1 = Integer.MAX_VALUE + 1900;
+	public static float ffloat1 = 33.99f;
+	public static double ddouble1 = 777.999;
+
+	public static byte bbyte2 = 12;
+	public static short shshort2 = 22;
+	public static int iint2 = 102;
+	public static long llong2 = Integer.MAX_VALUE + 10;
+	public static float ffloat2 = 22.99f;
+	public static double ddouble2 = 99.999;
+
+	// primitives short
+	public static byte b = 10;
+	public static short sh = 20;
+	public static int i = 100;
+	public static long l = Integer.MAX_VALUE + 900;
+	public static float f = 66.99f;
+	public static double d = 77.999;
+
+	public static byte b1 = 11;
+	public static short sh1 = 21;
+	public static int i1 = 111;
+	public static long l1 = Integer.MAX_VALUE + 1900;
+	public static float f1 = 33.99f;
+	public static double d1 = 777.999;
+
+	public static byte b2 = 12;
+	public static short sh2 = 22;
+	public static int i2 = 102;
+	public static long l2 = Integer.MAX_VALUE + 10;
+	public static float f2 = 22.99f;
+	public static double d2 = 99.999;
+
+	// Wrappers
+	public static Byte myByte = new Byte("112");
+	public static Short myShort = new Short("333");
+	public static Integer myInteger = new Integer("999");
+	public static Long myLong = new Long("876145");
+	public static Float myFloat = new Float(11.34f);
+	public static Double myDouble = new Double(777.98);
+
+	public static Byte myByte1 = new Byte("110");
+	public static Short myShort1 = new Short("331");
+	public static Integer myInteger1 = new Integer("998");
+	public static Long myLong1 = new Long("876146");
+	public static Float myFloat1 = new Float(12.32f);
+	public static Double myDouble1 = new Double(776.97);
+
+	public static Byte myByte2 = new Byte("107");
+	public static Short myShort2 = new Short("333");
+	public static Integer myInteger2 = new Integer("995");
+	public static Long myLong2 = new Long("876142");
+	public static Float myFloat2 = new Float(14.63f);
+	public static Double myDouble2 = new Double(772.92);
+
+	// BigInteger
+	public static BigInteger bigi = new BigInteger("99999");
+	public static BigInteger bigi1 = new BigInteger("99998");
+	public static BigInteger bigi2 = new BigInteger("99997");
+
+	// BigDecimal
+	public static BigDecimal bd = new BigDecimal("99999.99");
+	public static BigDecimal bd1 = new BigDecimal("99999.99");
+	public static BigDecimal bd2 = new BigDecimal("99999.99");
+
+	// String
+	public static String s = "my short string";
+	public static String s1 = "my first string";
+	public static String s2 = "my second string";
+	public static String s3 = "my third string";
+
+	public static String str = "This is my sample string w/o any Number";
+	public static String str1 = "This is my first string number 1";
+	public static String str2 = "This is my second string number 2";
+	public static String str3 = "This is my third string number 3";
+}
